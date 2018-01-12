3139e2c3ef9f3621a538d3dd0089c90c1bf2aea9
Jeremy <jeremyxu123@gmail.com>
2018-01-12 13:50:23 -0800

    inital commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const argv = require("yargs").argv;
+const command = argv._[0];
+if (command === "add") {
+  console.log("Add function chosen");
+}
+
+if (command = "remove") {
+  console.log("Remove function chosen");
+}
+
+
+if (command = "read") {
+  console.log("read function chosen");
+}
+
+if (command = "list") {
+  console.log("lsit function chosen");
+}
